2dd366a6bf5c5703410a0a7d3d9c64d4c013c1af
Allin Harmon <allinharmon@yahoo.com>
2013-07-09 09:14:48 -0400

    Full Site w Mobile

---
+++ js/toggle.js
@@ -0,0 +1,197 @@
+// JavaScript Document
+
+// add event listeners to #starters show/hide button
+	$('#starters').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#starters .menu-type').toggleClass('hide-menu');
+		$('#starters .menu-item').toggleClass('hide-menu');
+		$('#starters .menu-subtitle').toggleClass('hide-menu');
+		$('#starters .menu-description').toggleClass('hide-menu');
+		
+		
+	});
+	
+	// add event listeners to #starters show/hide button
+	$('#salads').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#salads .menu-type').toggleClass('hide-menu');
+		$('#salads .menu-item').toggleClass('hide-menu');
+		$('#salads .menu-subtitle').toggleClass('hide-menu');
+		$('#salads .menu-description').toggleClass('hide-menu');
+		
+		
+	});
+	
+	// add event listeners to #nachos show/hide button
+	$('#nachos').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#nachos .menu-type').toggleClass('hide-menu');
+		$('#nachos .menu-item').toggleClass('hide-menu');
+		$('#nachos .menu-subtitle').toggleClass('hide-menu');
+		$('#nachos .menu-description').toggleClass('hide-menu');
+		
+		
+	});
+	
+	// add event listeners to #burritos show/hide button
+	$('#burritos').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#burritos .menu-type').toggleClass('hide-menu');
+		$('#burritos .menu-item').toggleClass('hide-menu');
+		$('#burritos .menu-subtitle').toggleClass('hide-menu');
+		$('#burritos .menu-description').toggleClass('hide-menu');
+		
+		
+	});
+	
+	// add event listeners to #basicBurritos show/hide button
+	$('#basicBurritos').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#basicBurritos .menu-type').toggleClass('hide-menu');
+		$('#basicBurritos .menu-item').toggleClass('hide-menu');
+		$('#basicBurritos .menu-subtitle').toggleClass('hide-menu');
+		$('#basicBurritos .menu-description').toggleClass('hide-menu');
+		
+		
+	});
+	
+	// add event listeners to #basicBurritos show/hide button
+	$('#tacos').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#tacos .menu-type').toggleClass('hide-menu');
+		$('#tacos .menu-item').toggleClass('hide-menu');
+		$('#tacos .menu-subtitle').toggleClass('hide-menu');
+		$('#tacos .menu-description').toggleClass('hide-menu');
+		
+		
+	});
+	
+	// add event listeners to #basicBurritos show/hide button
+	$('#quesadillas').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#quesadillas .menu-type').toggleClass('hide-menu');
+		$('#quesadillas .menu-item').toggleClass('hide-menu');
+		$('#quesadillas .menu-subtitle').toggleClass('hide-menu');
+		$('#quesadillas .menu-description').toggleClass('hide-menu');
+		
+		
+	});
+	
+	// add event listeners to #forty-fives show/hide button
+	$('#forty-fives').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#forty-fives .menu-type').toggleClass('hide-menu');
+		$('#forty-fives .menu-item').toggleClass('hide-menu');
+		$('#forty-fives .menu-subtitle').toggleClass('hide-menu');
+		$('#forty-fives .menu-description').toggleClass('hide-menu');
+		
+		
+	});
+	
+	// add event listeners to #pasta show/hide button
+	$('#pasta').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#pasta .menu-type').toggleClass('hide-menu');
+		$('#pasta .menu-item').toggleClass('hide-menu');
+		$('#pasta .menu-subtitle').toggleClass('hide-menu');
+		$('#pasta .menu-description').toggleClass('hide-menu');
+				
+	});
+	
+	// add event listeners to #sides show/hide button
+	$('#sides').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#sides .menu-type').toggleClass('hide-menu');
+		$('#sides .menu-item').toggleClass('hide-menu');
+		$('#sides .menu-subtitle').toggleClass('hide-menu');
+		$('#sides .menu-description').toggleClass('hide-menu');
+				
+	});
+	
+	// add event listeners to #drinks show/hide button
+	$('#drinks').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#drinks .menu-type').toggleClass('hide-menu');
+		$('#drinks .menu-item').toggleClass('hide-menu');
+		$('#drinks .menu-subtitle').toggleClass('hide-menu');
+		$('#drinks .menu-description').toggleClass('hide-menu');
+				
+	});
+	
+	// add event listeners to #beer show/hide button
+	$('#beer').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#beer .menu-type').toggleClass('hide-menu');
+		$('#beer .menu-item').toggleClass('hide-menu');
+		$('#beer .menu-subtitle').toggleClass('hide-menu');
+		$('#beer .menu-description').toggleClass('hide-menu');
+				
+	});
+	
+	// add event listeners to #margaritas show/hide button
+	$('#margaritas').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#margaritas .menu-type').toggleClass('hide-menu');
+		$('#margaritas .menu-item').toggleClass('hide-menu');
+		$('#margaritas .menu-subtitle').toggleClass('hide-menu');
+		$('#margaritas .menu-description').toggleClass('hide-menu');
+				
+	});
+	
+	
+	// add event listeners to #spirits show/hide button
+	$('#spirits').click(function(e){
+		e.preventDefault();
+		var link = this;
+		
+		// changes button status
+		$('#spirits .menu-type').toggleClass('hide-menu');
+		$('#spirits .menu-item').toggleClass('hide-menu');
+		$('#spirits .menu-subtitle').toggleClass('hide-menu');
+		$('#spirits .menu-description').toggleClass('hide-menu');
+				
+	});
+	
+	
+	
+	
+	
